ae7a30d23d9b173c689b2b9066e444c7dea9a617
Michael Kleyman <michaelkleyman99@gmail.com>
2023-03-27 19:33:25 -0400

    commit needed to pull

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-const express = require('express')
-const cors = require('cors')
+const express = require('express');
+const cors = require('cors');
 
-const app = express()
+const app = express();
 
-const AppRouter = require('./routes/AppRouter')
+const AppRouter = require('./routes/AppRouter');
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 3001;
 
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => res.json({ message: 'Server Works' }))
-app.use('/quackBack', AppRouter)
-app.listen(PORT, () => console.log(`Server Started On Port: ${PORT}`))
+app.get('/', (req, res) => res.json({ message: 'Server Works' }));
+// app.use('/quackBack', AppRouter)
+app.listen(PORT, () => console.log(`Server Started On Port: ${PORT}`));
