99e0cca0ccb0359c8cdf5255e1f182332a0a7125
Patrick Knight <pfknight2@gmail.com>
2023-03-31 16:28:14 -0400

    edited planet routes from .json to .send

--- routes/PlanetRouter.js
+++ routes/PlanetRouter.js
@@ -3,7 +3,7 @@ const controller = require('../controllers/PlanetController')
 
 Router.post('/', controller.addPlanet)
 Router.get('/:name', controller.getPlanetByName)
-Router.get('/moons/:planet_id', controller.getPlanetWithMoons)
+Router.get('/moons/:name', controller.getPlanetWithMoons)
 Router.put('/:planet_id', controller.updatePlanet)
 Router.delete('/:planet_id', controller.deletePlanet)
 
