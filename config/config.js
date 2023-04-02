c733f4c570fb495e40e84b257a279d730b08de60
Patrick Knight <pfknight2@gmail.com>
2023-04-02 14:08:05 -0400

    changed config to js based file

---
+++ config/config.js
@@ -0,0 +1,15 @@
+require('dotenv').config()
+module.exports = {
+  development: {
+    database: 'solarSystemQuack',
+    dialect: 'postgres'
+  },
+  test: {
+    database: 'solarSystemQuack_test',
+    dialect: 'postgres'
+  },
+  production: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres'
+  }
+}
